d794e6ad100ee4737622bc7a21958b07e8e93266
Miharimamy RAJAONISON <mirajaonison@yahoo.com>
2021-04-28 22:06:06 +0200

    remove inspection

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # python3
 import wx
 import main_window
-import wx.lib.inspection
 app = wx.App()
 configurationWindow = main_window.MainWindow(None, title="Mouna", size=wx.Size(800, 600))
 configurationWindow.Show()
-wx.lib.inspection.InspectionTool().Show()
 app.MainLoop()
